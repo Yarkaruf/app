7556a0dda314ce5f270dcb921ff5f2e9d50432f2
Yarkaruf <yaroslav.ershov.02@mail.ru>
2022-03-24 17:10:59 +0800

    3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
+import Head from 'next/head'
+import React, { Fragment } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return <Fragment>
+    <Head>
+      <title>MyTop</title>
+      <link key={2} rel="icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </Fragment>
 }
 
 export default MyApp
